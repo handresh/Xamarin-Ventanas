5df60d68af4c0ca39e7f505b0835065cca5090ff
andres2050h1 <andres2050h@gmail.com>
2020-05-23 18:33:37 -0500

    XAML Elementos

---
+++ Forms1/Forms1/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Forms1
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int count = 0;
+
+        private void BtnSumar_Clicked(object sender, EventArgs e)
+        {
+            count++;
+            btnSumar.Text = count.ToString();
+        }
+
+        private void BtnSave_Clicked(object sender, EventArgs e)
+        {
+            var nombre = boxNombre.Text;
+            if (!String.IsNullOrEmpty(nombre)) {
+                DisplayAlert("Xamarin Forms", nombre, "Aceptar");
+            }
+        }
+    }
+}
